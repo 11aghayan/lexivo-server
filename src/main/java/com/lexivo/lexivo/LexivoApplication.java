83824f1662a95df287d022753ce79d34038592b7
11aghayan <11.aghayan.99@gmail.com>
2025-08-17 20:38:35 +0200

    first commit

---
+++ src/main/java/com/lexivo/lexivo/LexivoApplication.java
@@ -0,0 +1,13 @@
+package com.lexivo.lexivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexivo/lexivo/LexivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexivo.lexivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
